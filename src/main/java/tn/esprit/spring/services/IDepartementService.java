745323c8bf72aac6794f038c6ed152a9e41dacc7
Yasmine Nasfi <yasmine96nasfi@gmail.com>
2020-10-19 21:39:49 +0200

    "Yasmine Test"

--- src/main/java/tn/esprit/spring/services/IDepartementService.java
+++ src/main/java/tn/esprit/spring/services/IDepartementService.java
@@ -10,7 +10,11 @@ public interface IDepartementService {
 	
 	public List<Departement> getAllDepartements();
 
-
+	public Departement addDepartement(Departement d);
+	public Departement UpdateDepartement(Departement d);
+	public void deleteDepartement(String id);
+	public Departement retreiveDepartement(String id);
+	
 	
 	
 	
